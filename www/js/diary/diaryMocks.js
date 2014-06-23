7fc9ffc3faab1d4824891887311ea405ee6044e8
Jeremy Hewett <jeremyhewett@gmail.com>
2014-06-22 18:37:19 -0700

    Added diary tab using local storage to store entries.

---
+++ www/js/diary/diaryMocks.js
@@ -0,0 +1,20 @@
+var diaryMockData = {
+    entries: [
+        {
+            timeStamp: new Date().getTime(),
+            narrative: 'This is my latest entry.'
+        },
+        {
+            timeStamp: Date.parse('June 20, 2014'),
+            narrative: 'This is my 2nd entry.'
+        },
+        {
+            timeStamp: Date.parse('June 19, 2014'),
+            narrative: 'This is my first entry.'
+        },
+        {
+            timeStamp: Date.parse('June 19, 2014'),
+            narrative: 'Another entry.'
+        }
+    ]
+};
